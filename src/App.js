e33d607897cd625cff7364e24f1b85857aecf9e0
shramish2057 <shramish4@gmail.com>
2021-02-23 22:17:54 +0100

    Making changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LoginComponent from './Login/login';
+import SignupComponent from './Signup/signup';
+import DashboardComponent from './Dashboard/dashboard';
+const firebase = require("firebase");
+require("firebase/firestore"); // Required for side-effects
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AppContainer extends React.Component {
+
+  componentWillMount() {
+    this.checkForSavedAuth();
+  }
+
+  // constructor() {
+  //   super();
+  //   firebase.initializeApp({
+  //     apiKey: "AIzaSyAlWBvbvLv7dT6_RYnlCeZbOcotpeBU3Y8",
+  //     authDomain: "im-app-tutorial.firebaseapp.com",
+  //     databaseURL: "https://im-app-tutorial.firebaseio.com",
+  //     projectId: "im-app-tutorial",
+  //     storageBucket: "im-app-tutorial.appspot.com",
+  //     messagingSenderId: "199544684635",
+  //     appId: "1:199544684635:web:fb388e2c181f0476"
+  //   });
+  //   this.db = firebase.firestore();
+  //   this.state = {
+  //     user: null
+  //   };
+  // }
+
+  render() {
+    if(this.state.user)
+      return <DashboardComponent user={this.state.user}></DashboardComponent>
+    else
+      return <SignupComponent loginFn={this.loggedIn}></SignupComponent>
+  }
+
+  checkForSavedAuth = () => {
+    console.log(firebase.auth().currentUser);
+  };
+
+  loggedIn = (user) => this.setState({ user: user });
+
 }
 
-export default App;
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import LoginComponent from './Login/login';
+import SignupComponent from './Signup/signup';
+import DashboardComponent from './Dashboard/dashboard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const firebase = require("firebase");
+require("firebase/firestore"); // Required for side-effects?????
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAlWBvbvLv7dT6_RYnlCeZbOcotpeBU3Y8",
+  authDomain: "im-app-tutorial.firebaseapp.com",
+  databaseURL: "https://im-app-tutorial.firebaseio.com",
+  projectId: "im-app-tutorial",
+  storageBucket: "im-app-tutorial.appspot.com",
+  messagingSenderId: "199544684635",
+  appId: "1:199544684635:web:fb388e2c181f0476"
+});
+
+const routing = (
+  <Router>
+    <div id='routing-container'>
+      <Route path='/login' component={LoginComponent}></Route>
+      <Route path='/signup' component={SignupComponent}></Route>
+      <Route path='/dashboard' component={DashboardComponent}></Route>
+    </div>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
