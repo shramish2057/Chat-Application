1c29954471e62b3a22000996524df260c802764d
shramish2057 <shramish4@gmail.com>
2021-02-23 22:43:51 +0100

    Making changes

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ class AppContainer extends React.Component {
     this.checkForSavedAuth();
   }
 
-  // constructor() {
-  //   super();
-  //   firebase.initializeApp({
-  //     apiKey: "AIzaSyAlWBvbvLv7dT6_RYnlCeZbOcotpeBU3Y8",
-  //     authDomain: "im-app-tutorial.firebaseapp.com",
-  //     databaseURL: "https://im-app-tutorial.firebaseio.com",
-  //     projectId: "im-app-tutorial",
-  //     storageBucket: "im-app-tutorial.appspot.com",
-  //     messagingSenderId: "199544684635",
-  //     appId: "1:199544684635:web:fb388e2c181f0476"
-  //   });
-  //   this.db = firebase.firestore();
-  //   this.state = {
-  //     user: null
-  //   };
-  // }
+  constructor() {
+    super();
+    firebase.initializeApp({
+    apiKey: "AIzaSyDXvBQV_gcoCgJ4aymNlSjpbpPW2oumPTo",
+    authDomain: "chatapp-11d96.firebaseapp.com",
+    projectId: "chatapp-11d96",
+    storageBucket: "chatapp-11d96.appspot.com",
+    messagingSenderId: "718686552404",
+    appId: "1:718686552404:web:1aee4e1e23d827e0b8c8c1",
+    measurementId: "G-6JH6G5MK1C"
+    });
+    this.db = firebase.firestore();
+    this.state = {
+      user: null
+    };
+}
 
   render() {
     if(this.state.user)
